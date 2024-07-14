2e38df8ff17485659cd7a23d21283f7cae39716a
Szeri <szeri323@gmail.com>
2024-07-14 12:45:59 +0200

    add break walls logic

--- maze.py
+++ maze.py
@@ -1,8 +1,9 @@
 from cell import Cell
 import time
+import random
 
 class Maze:
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None):
+    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win=None, seed=None):
         self._cells = []
         self._x1 = x1
         self._y1 = y1
@@ -11,10 +12,16 @@ class Maze:
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win 
+        if seed is not None:
+            self.seed = random.seed(seed)
         
         self._create_cells()
-        
         self._break_entrance_and_exit()
+        self._reset_cells_visited()
+        
+        for i in range(self._num_rows):
+            for j in range(self._num_cols):
+                self._break_walls_r(i, j)
         
     def _create_cells(self):
         for row in range(self._num_rows):
@@ -46,3 +53,46 @@ class Maze:
         self._draw_cell(0, 0)
         self._cells[self._num_rows-1][self._num_cols-1].has_bottom_wall = False
         self._draw_cell(self._num_rows-1, self._num_cols-1)
+        
+    def _break_wall(self, i, j, di, dj):
+        if (di, dj) == (-1, 0):
+            self._cells[i][j].has_top_wall = False
+            self._cells[i-1][j].has_bottom_wall = False
+        elif (di, dj) == (1, 0):
+            self._cells[i][j].has_bottom_wall = False
+            self._cells[i+1][j].has_top_wall = False
+        elif (di, dj) == (0, -1):
+            self._cells[i][j].has_left_wall = False
+            self._cells[i][j-1].has_right_wall = False
+        elif (di, dj) == (0, 1):
+            self._cells[i][j].has_right_wall = False
+            self._cells[i][j+1].has_left_wall = False
+                
+        self._draw_cell(i, j)
+    
+    def _break_walls_r(self, i, j):
+        self._cells[i][j].visited = True
+        while True:
+            to_visit = []
+            directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+            
+            for di, dj in directions:
+                ni, nj = i + di, j + dj
+                if 0 <= ni < len(self._cells) and 0 <= nj < len(self._cells[0]):
+                    if not self._cells[ni][nj].visited:
+                        to_visit.append((ni, nj, di, dj))
+
+            if len(to_visit) == 0:
+                self._draw_cell(i, j)
+                return
+            else:
+                ni, nj, di, dj = random.choice(to_visit)
+                self._break_wall(i, j, di, dj)
+                
+                self._break_walls_r(ni, nj)
+        
+    def _reset_cells_visited(self):
+        for i in range(self._num_rows-1):
+            for j in range(self._num_cols-1):
+                self._cells[i][j].visited = False
+                print(self._cells[i][j].visited)
